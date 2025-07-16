dfc9b7529f3f0e587f01be156dfcc9235dface41
Pedro Converso D'andrea <166546777+PedroConverso@users.noreply.github.com>
2025-07-16 09:47:00 -0300

    arreglar pinchos

---
+++ Assets/Scripts/restartOnFall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartOnFall : MonoBehaviour
+{
+    public float alturaCaida = -10;
+
+    void Update()
+    {
+        if(transform.position.y < alturaCaida){
+            SceneManager.LoadScene("Parkur");
+        }
+    }
+}
